61d612bcaa1ed56522d52c3d0c140cf1e6c4c82a
leorigaudeau <leo.rigaudeau@ynov.com>
2020-02-13 00:29:57 +0100

    ajout du tir

--- js/page/fight.js
+++ js/page/fight.js
@@ -1,12 +1,39 @@
 class Fight {
   constructor(game) {
     this.game = game;
+    // donne la derniere direction du personnage
+    this.lastDirection = "up";
+    this.bulletSettings = [];
+    this.bulletSize=16
 
     this.update = game => {
       this.game = game;
+      this.game.keys.forEach((key, id) => {
+        this.game.lastKeys.forEach((lastkey, lastid) => {
+          if (id === lastid) {
+            if (id === "a" && lastid === "a" && key) {
+              this.throwBullet();
+              
+            }
+            if (id === "up" && lastid === "up" && key) {
+              this.lastDirection = "up";
+            }
+            if (id === "down" && lastid === "down" && key) {
+              this.lastDirection = "down";
+            }
+            if (id === "left" && lastid === "left" && key) {
+              this.lastDirection = "left";
+            }
+            if (id === "right" && lastid === "right" && key) {
+              this.lastDirection = "right";
+            }
+          }
+        });
+      });
       console.log("rankk");
     };
-    this.updateDisplay = display => {
+    this.updateDisplay = display => {;
+      this.display = display;
       var player = display.game.player;
 
       display.cx.drawImage(
@@ -46,6 +73,47 @@ class Fight {
         10 * display.zoom,
         10 * display.zoom
       );
+
+      var spliceArray=[];
+      this.bulletSettings.forEach((bullet, index) => {
+        display.cx.drawImage(display.playerSprite, bullet.x, bullet.y, this.bulletSize, this.bulletSize);
+        if (
+          bullet.x > display.canvas.width ||
+          bullet.x < 0 ||
+          bullet.y > display.canvas.height ||
+          bullet.y < 0
+        ) {
+          this.bulletSettings.splice(index, 1);
+        } else {
+          switch (bullet.direction) {
+            case "up":
+              bullet.y -= this.bulletSize;
+              break;
+
+            case "down":
+              bullet.y += this.bulletSize;
+              break;
+
+            case "right":
+              bullet.x += this.bulletSize;
+              break;
+
+            case "left":
+              bullet.x -= this.bulletSize;
+              break;
+
+            default:
+              break;
+          }
+        }
+      });
+    };
+    this.throwBullet = () => {
+      this.bulletSettings.push({
+        x: this.game.player.pos.x* this.display.zoom + (this.game.player.size.x/2-this.bulletSize/2),
+        y: this.game.player.pos.y* this.display.zoom+(this.game.player.size.x/2-this.bulletSize/2),
+        direction: this.lastDirection
+      });
     };
   }
 }
